6581312a5fda71e1d8cac13a0d08e7acf0be717e
Caio Fauza <caiofauza@gmail.com>
2019-04-15 18:50:08 -0300

    Aps3 completa

--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -1,54 +1,79 @@
 package br.pro.hashi.ensino.desagil.aps.view;
 
 import br.pro.hashi.ensino.desagil.aps.model.Gate;
+import br.pro.hashi.ensino.desagil.aps.model.Light;
 import br.pro.hashi.ensino.desagil.aps.model.Switch;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
 
-public class GateView extends JPanel implements ItemListener {
+public class GateView extends FixedPanel implements ItemListener, MouseListener {
     private final Switch[] switches;
     private final Gate gate;
+    private final Light light;
+
 
     private final JCheckBox[] inputBoxes;
-    private final JCheckBox outputBox;
+
+    private final Image image;
+
 
     public GateView(Gate gate) {
+        super(345, 300);
         this.gate = gate;
 
         int inputSize = gate.getInputSize();
-
         switches = new Switch[inputSize];
         inputBoxes = new JCheckBox[inputSize];
+        light = new Light();
 
         for (int i = 0; i < inputSize; i++) {
             switches[i] = new Switch();
             inputBoxes[i] = new JCheckBox();
-
             gate.connect(i, switches[i]);
+            light.connect(0, gate);
         }
 
-        outputBox = new JCheckBox();
+        light.setR(255);
+
 
         JLabel inputLabel = new JLabel("Input");
         JLabel outputLabel = new JLabel("Output");
 
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         add(inputLabel);
+        int counter = -1;
         for (JCheckBox inputBox : inputBoxes) {
-            add(inputBox);
+            counter += 1;
+            if (inputSize == 1) {
+                add(inputBox, 60, 113, 25, 25);
+            } else {
+                if (counter == 0) {
+                    add(inputBox, 60, 85, 25, 25);
+                } else {
+                    add(inputBox, 60, 128, 25, 25);
+                }
+
+
+            }
+
         }
         add(outputLabel);
-        add(outputBox);
+
 
         for (JCheckBox inputBox : inputBoxes) {
             inputBox.addItemListener(this);
         }
 
-        outputBox.setEnabled(false);
-
+        String name = gate.toString() + ".png";
+        URL url = getClass().getClassLoader().getResource(name);
+        image = getToolkit().getImage(url);
+        addMouseListener(this);
         update();
     }
 
@@ -60,14 +85,61 @@ public class GateView extends JPanel implements ItemListener {
                 switches[i].turnOff();
             }
         }
+        repaint();
 
-        boolean result = gate.read();
 
-        outputBox.setSelected(result);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        int x = event.getX();
+        int y = event.getY();
+        Color temporaryColor;
+
+        if (Math.pow((x - 275.5), 2) + Math.pow((y - 122.5), 2) < 156.25) {
+            //x coordinate + radius, y coordinate + radius, radius^2
+            temporaryColor = JColorChooser.showDialog(this, null, new Color(light.getR(), light.getG(), light.getB()));
+            try {
+                light.setR(temporaryColor.getRed());
+                light.setG(temporaryColor.getGreen());
+                light.setB(temporaryColor.getBlue());
+
+            } catch (Exception ex) {
+
+            }
+
+
+            repaint();
+        }
     }
 
     @Override
     public void itemStateChanged(ItemEvent event) {
         update();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent event) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent event) {
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 80, 80, 179, 87, this);
+        g.setColor(new Color(light.getR(), light.getG(), light.getB()));
+        g.fillOval(263, 110, 25, 25);
+        getToolkit().sync();
     }
 }
